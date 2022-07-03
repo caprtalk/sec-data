ed97d77d234dae4c19d2bc2254a16c22807becd6
Caspar Brown <casparmoab@gmail.com>
2022-07-03 16:53:42 -0600

    changes to reading

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import os
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 import load_filings
 
 
-def read_filing(file):
-    tree = ET.parse(file)
-    root = tree.getroot()
-    print(root[5])
+def read_filing(file):  # function to read xml files
+    # parse xml file
+    infile = open(file)
+    contents = infile.read()
+    soup = BeautifulSoup(contents, 'xml')
+    # pull filing data from root
+    titles = soup.find_all('transactionShares')
+    thing = 0
+    for title in titles:
+        title = int(title)
+        thing += title
+    return thing
 
 
 def main():
     num_files = load_filings.load_filings()
     path = os.path.dirname(__file__)
     count = 1
+    data = []
     while count <= num_files:
-        read_filing(f'{path}/temp-sec-filings/filing-{count}.xml')
+        filing_data = read_filing(f'{path}/temp-sec-filings/filing-{count}.xml')
+        data.append(filing_data)
         count += 1
     load_filings.clear_filings()
+    print(data)
 
 
 if __name__ == '__main__':
